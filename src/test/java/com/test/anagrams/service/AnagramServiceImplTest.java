aa68e38845a17863f9525866434ab5db3cdf2687
LAPTOP-E215GRSA\hpatel <hiralbest@gmail.com>
2020-05-26 22:00:04 -0400

    Test cases for strings are anagrams or not.

---
+++ src/test/java/com/test/anagrams/service/AnagramServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.test.anagrams.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AnagramServiceImplTest {
+
+    AnagramService anagramService;
+
+    @BeforeEach
+    void setUp() {
+        anagramService = new AnagramServiceImpl();
+    }
+
+    @Test
+    @DisplayName("Not an anagram when string sizes are different")
+    void isAnagram_when_string_size_is_different() {
+        String s1 = "abc";
+        String s2 = "abcd";
+        assertFalse(anagramService.isAnagram(s1, s2));
+
+    }
+
+    @Test
+    @DisplayName("Strings are anagrams")
+    void isAnagram_when_strings_are_anagram(){
+        String s1 = "abc";
+        String s2 = "bca";
+        assertTrue(anagramService.isAnagram(s1, s2));
+
+        s2 = "cba";
+        assertTrue(anagramService.isAnagram(s1, s2));
+    }
+
+    @Test
+    @DisplayName("Strings are not anagrams")
+    void isAnagram_when_strings_are_not_anagram(){
+        String s1 = "abc";
+        String s2 = "bda";
+        assertFalse(anagramService.isAnagram(s1, s2));
+
+        s2 = "uhg";
+        assertFalse(anagramService.isAnagram(s1, s2));
+    }
+}
