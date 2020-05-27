c1f08497b83e94b0a3daa17e91e8f75c57731099
LAPTOP-E215GRSA\hpatel <hiralbest@gmail.com>
2020-05-27 17:29:17 -0400

    Test of get all anagrams.

--- src/test/java/com/test/anagrams/service/AnagramServiceImplTest.java
+++ src/test/java/com/test/anagrams/service/AnagramServiceImplTest.java
@@ -1,9 +1,11 @@
 package com.test.anagrams.service;
 
+import com.google.common.collect.Sets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -28,7 +30,7 @@ public class AnagramServiceImplTest {
 
     @Test
     @DisplayName("Strings are anagrams")
-    void isAnagram_when_strings_are_anagram(){
+    void isAnagram_when_strings_are_anagram() {
         String s1 = "abc";
         String s2 = "bca";
         assertTrue(anagramService.isAnagram(s1, s2));
@@ -39,12 +41,67 @@ public class AnagramServiceImplTest {
 
     @Test
     @DisplayName("Strings are not anagrams")
-    void isAnagram_when_strings_are_not_anagram(){
+    void isAnagram_when_strings_are_not_anagram() {
         String s1 = "abc";
         String s2 = "bda";
         assertFalse(anagramService.isAnagram(s1, s2));
 
         s2 = "uhg";
         assertFalse(anagramService.isAnagram(s1, s2));
+    }
+
+    @Test
+    @DisplayName("Correct anagrams for string length of 1")
+    void getAllAnagramStrings_when_string_1char() {
+        String s1 = "a";
+        Set<String> s1_expected_anagrams = Sets.newHashSet();
+        Set<String> s1_actual_anagrams = anagramService.getAllAnagramStrings(s1);
+        assertTrue(isEqualSet(s1_actual_anagrams, s1_expected_anagrams), "Correct anagrams for string length of 1");
+    }
+
+    @Test
+    @DisplayName("Correct anagrams for string length of 2")
+    void getAllAnagramStrings_when_string_2char() {
+        String s1 = "ab";
+        Set<String> s1_expected_anagrams = Sets.newHashSet("ba");
+        Set<String> s1_actual_anagrams = anagramService.getAllAnagramStrings(s1);
+        assertTrue(isEqualSet(s1_actual_anagrams, s1_expected_anagrams), "Correct anagrams for string length of 2");
+    }
+
+    @Test
+    @DisplayName("Correct anagrams for string length of 3")
+    void getAllAnagramStrings_when_string_3char() {
+        String s1 = "abc";
+        Set<String> s1_expected_anagrams = Sets.newHashSet("acb", "bac", "bca", "cab", "cba");
+        Set<String> s1_actual_anagrams = anagramService.getAllAnagramStrings(s1);
+        assertTrue(isEqualSet(s1_actual_anagrams, s1_expected_anagrams), "Correct anagrams for string length of 3");
+    }
+
+    @Test
+    @DisplayName("Correct anagrams for string with duplicate character")
+    void getAllAnagramStrings_when_string_with_duplicate_character() {
+        String s1 = "aba";
+        Set<String> s1_expected_anagrams = Sets.newHashSet("baa", "aab");
+        Set<String> s1_actual_anagrams = anagramService.getAllAnagramStrings(s1);
+        assertTrue(isEqualSet(s1_actual_anagrams, s1_expected_anagrams), "Correct anagrams for string with duplicate character");
+    }
+
+    /**
+     * Reference: https://mkyong.com/java/java-how-to-compare-two-sets/
+     *
+     * @param set1
+     * @param set2
+     * @return
+     */
+    boolean isEqualSet(Set<String> set1, Set<String> set2) {
+        if (set1 == null || set2 == null) {
+            return false;
+        }
+
+        if (set1.size() != set2.size()) {
+            return false;
+        }
+
+        return set1.containsAll(set2);
     }
 }
